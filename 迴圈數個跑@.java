b524b09b21172ab16fcecefedcb29a65fc8b108e
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2018-10-24 14:51:04 +0800

    Add files via upload

---
+++ 迴圈數個跑@.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int result=0;
+        for (int i=1;i<=a;i++){
+            result=result+i;
+            System.out.print("@");
+        }
+    }
+}
