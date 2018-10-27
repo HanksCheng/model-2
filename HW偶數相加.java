6bd14fe06d10d60ac8fed29b7f31e56f0a355161
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2018-10-27 21:24:34 +0800

    Add files via upload

---
+++ HW偶數相加.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        int result=0;
+        if (a%2!=0){
+            a++;
+        }
+        for (int i=a;i<=b;i+=2){
+            result=result+i;
+        }
+        System.out.println(result);
+    }
+}
--- HW季節判斷.java
+++ HW季節判斷.java
@@ -3,21 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String v1 = scn.next();
-        switch (v1){
+        Scanner scn=new Scanner(System.in);
+        String a=scn.next();
+        switch (a){
             case "Spring":
                 System.out.println("2~4");
-                break;
-            case "Summer":
-                System.out.println("5~7");
-                break;
-            case "Fall":
-                System.out.println("8~10");
-                break;
-            case "Winter":
-                System.out.println("11~1");
-                break;
         }
     }
 }
