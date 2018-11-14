77d63e4c13f517bb1eeb0160528b0b6487809324
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2018-11-14 14:18:02 +0800

    Update 10進位轉2進位.java

--- 10進位轉2進位.java
+++ 10進位轉2進位.java
@@ -7,10 +7,14 @@ public class Main {
         int n=scn.nextInt();
         String str="";
         while (n>=2){
-            str=str+Integer.toString(n%2);
+            str=Integer.toString(n%2)+str;
             n=n/2;
         }
         str="1"+str;
+        int length=str.length();
+        for(int i=0;i<8-length;i++){
+            str="0"+str;
+        }
         System.out.println(str);
     }
 }
